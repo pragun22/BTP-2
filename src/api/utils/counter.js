009477a11d4c4d355347772df8439bb1f45b8a52
pragun22 <saxenapragun@gmail.com>
2020-10-19 11:33:35 +0530

    starting again

--- src/api/utils/counter.js
+++
@@ -1,8 +0,0 @@
-function* counter() {
-  let index = 0;
-  while (index < Number.MAX_SAFE_INTEGER) {
-    yield index++;
-  }
-}
-
-export default counter;
--- src/constants/index.js
+++
@@ -1,5 +0,0 @@
-export const INCLUDE_UNFINISHED_FEATURES = false;
-
-export default {
-  INCLUDE_UNFINISHED_FEATURES
-};
